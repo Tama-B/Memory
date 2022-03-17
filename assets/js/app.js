6125db9a74aa1d015b605aca65685b7c5f75d9b9
Tama-B <tama.britsch@gmail.com>
2022-03-17 13:12:31 +0100

    soweit fertig!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 const section = document.querySelector('section');
 const roundCounterDisplay = document.querySelector('#round_count');
-let timerDisplay = document.querySelector('#time');
+const timerDisplay = document.querySelector('#time');
+const startGameBtn = document.querySelector('#startBtn');
+const cardBlocker = document.querySelector('#card_blocker');
+const roundStats = document.querySelector('#stats_container');
+const tryStats = document.querySelector('#round_stats p:first-of-type');
+const timeStats = document.querySelector('#round_stats p:last-of-type');
 let roundCounter = 0;
 let flippedCard1;
 let flippedCard2;
@@ -20,15 +25,69 @@ let cardData = [
     { imgSrc: '/assets/img/underwater.png', name: 'underwater' }
 ]
 
-
+//double card data so you have card pairs
+const doubleData = () => cardData = [...cardData, ...cardData]
+doubleData()
 
 //Randomize Card Data
 const randomizeData = () => cardData.sort(() => Math.random() - 0.5);
 randomizeData()
 
-//double date data so you have card pairs
-const doubleData = () => cardData = [...cardData, ...cardData]
-doubleData()
+
+//Track the time you need to finish the memory
+
+let interval
+let elapsedTime
+
+const startStopWatch = () => {
+    const startTime = new Date();
+    interval = setInterval(() => {
+        let currentTime = new Date();
+        elapsedTime = new Date(currentTime - startTime);
+        timerDisplay.textContent = elapsedTime.toLocaleTimeString().slice(3);
+    }, 1000)
+}
+
+const stopStopWatch = () => {
+    if (foundPairs.length == cardData.length) {
+        resetGame()
+        clearInterval(interval);
+
+    }
+}
+
+//Start the game via button
+let gameCounter = 0;
+
+startTime = new Date();
+const startGame = () => {
+    gameCounter++
+    console.log(gameCounter);
+    if (gameCounter > 1) {
+        roundCounterDisplay.textContent = 0;
+        allCards.forEach(element => {
+            element.remove();
+        });
+        randomizeData();
+        createCards();
+    }
+    startStopWatch();
+    cardBlocker.classList.add('hide');
+}
+startGameBtn.addEventListener('click', () => { startGame() });
+
+//Restart the game
+const resetGame = () => {
+    cardBlocker.classList.remove('hide');
+    startGameBtn.textContent = 'Play again';
+    foundPairs = [];
+    roundStats.classList.remove('hide');
+    tryStats.textContent = `Runden: ${roundCounter}`;
+    timeStats.textContent = ` Zeit: ${elapsedTime.toLocaleTimeString().slice(3)}`
+    roundCounter = 0;
+}
+
+
 
 //make sure, you can only flip two cards 
 let flippedCardsCounter = 0;
@@ -57,7 +116,7 @@ resetCards = () => {
     });
 }
 
-
+//Count how many rounds you need to finish the game
 let countRounds = () => {
     roundCounter++
     roundCounterDisplay.innerText = roundCounter;
@@ -73,6 +132,7 @@ const flipCard = (card) => {
         card.status = 'flipped';
         blockFlippedCards();
         flippedCard1 = card;
+
     }
     if (flippedCardsCounter == 2 && card.status == 'flipped') {
         flippedCardsCounter = 1;
@@ -84,11 +144,9 @@ const flipCard = (card) => {
         blockFlippedCards();
         flippedCard2 = card;
         compareCards(flippedCard1, flippedCard2);
+        stopStopWatch()
     }
 }
-
-
-
 
 // Create Cards
 const createCards = () => {
@@ -109,8 +167,7 @@ const createCards = () => {
 
     });
 }
-
-createCards()
+document.addEventListener("DOMContentLoaded", createCards())
 
 //Compare if same card or not
 const compareCards = (card1, card2) => {
@@ -135,6 +192,16 @@ const compareCards = (card1, card2) => {
         }, 1000)
     }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
